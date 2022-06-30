c6fad87b2f4c6515cdfedf9ff9b074fb41c1dd37
JJ Vega <69365848+jjvega86@users.noreply.github.com>
2022-06-30 16:55:04 -0400

    Added expected output and wrap up question

--- sql_in_action-starter.sql
+++ sql_in_action-starter.sql
@@ -5,32 +5,47 @@
 
 -- Problem 1
 -- Find out how many rows are in the table "airbnb_listings_nyc"
+-- EXPECTED OUTPUT: 146
 
 -- Problem 2
 -- Find out the name of the host for "host_id" 63613
--- HINT: Where could it be?
+-- HINT: "Where" could it be?
+
+-- EXPECTED OUTPUT: Patricia
+
 
 -- Problem 3
 -- Query the data to just show the unique neighborhoods listed
--- HINT: This is a distinct operation...
+-- HINT: This is a "distinct" operation...
+
+-- EXPECTED OUTPUT: 40 neighborhoods listed
+
 
 -- Problem 4
 -- Find both the highest price listing and the lowest price listing, displaying the entire row for each
--- HINT: This can be two different queries
+-- HINT: This can be two different queries.
 
 -- FOOD FOR THOUGHT: Think about the results. Are the high and low prices outliers in this data set?
+
+-- EXPECTED OUTPUT: Highest = 785, Lowest = 55
 
 -- Problem 5
 -- Find the average availability for all listings in the data set (using the availability_365 column)
 -- HINT: Aggregates are more than just big rocks...
 
+-- EXPECTED OUTPUT: 165.3904
+
 -- Problem 6
 -- Find all listings that do NOT have a review
 -- HINT: There are a few ways to go about this. Remember that an empty cell is "no value", but not necessarily NULL
 
+-- EXPECTED OUTPUT: 6 rows
+
 -- Problem 7
 -- Find the id of the listing with a room_type of "Private room" that has the most reviews 
 -- HINT: Sorting is your friend!
+
+-- EXPECTED OUTPUT: 58059
 
 
 -- Problem 8
@@ -38,9 +53,13 @@
 -- HINT: Look for which neighborhood appears most frequently in the neighbourhood column
 -- HINT: You are creating "summary rows" for each neighborhood, so you will just see one entry for each neighborhood
 
+-- EXPECTED OUTPUT: Williamsburg
+
 -- Problem 9
 -- Query the data to discover which listing is the most popular using the reviews_per_month for all listings with a minimum_nights value of less than 7
 -- HINT: Sorting is still your friend! So are constraints.
+
+-- EXPECTED OUTPUT: 58059
 
 -- Problem 10
 -- Find out which host has the most listings. 
@@ -48,9 +67,12 @@
 -- Display the column using aliasing.
 -- HINT: Work this one step at a time. See if you can find a way to just display the count of listings per host first.
 
+-- EXPECTED OUTPUT: The Box House Hotel with 6 listings
 
 -- BONUS
 -- Find the the percent above or below each listing is compared to the average price for that neighborhood.
 -- HINT: No hints! It's a bonus for a reason :)
 
-
+-- WRAP UP
+-- What do you think makes a successful AirBnB rental in this market? What factors seem to be at play the most?
+-- Write a few sentances and include them with your project submission
