4fe0799c1f203630c5a7fa3c42d4894f88ab5281
JJ <jj.vega86@gmail.com>
2022-07-01 10:48:50 -0400

    Reformatted script file + added starter content to README

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# DA_SQL_In_Action_Starter
-Starter files for the SQL in Action project; Data Analytics Flex; Week 6
+# SQL In Action - Data Analytics Flex
+
+Project problems and setup directions
+
+## PREREQUISITES
+
+1. MySQL Server and MySQL Workbench, installed on your computer
+2. Basic understanding of SQL queries
+   1. SELECT, SELECT DISTINCT
+   2. WHERE, operators with WHERE, complex expressions using AND, OR, NOT, and NULL
+   3. Filtering and sorting (LIMIT, ORDER BY)
+   4. Aggregate functions: COUNT, MIN, MAX, AVG, SUM, GROUP BY
+   5. Aliasing (creating a new temporary column with the AS clause)
+
+## SETUP
+
+---
+
+1. Download the `.zip` file for this GitHub repository
+2. Unzip the file on your computer
+3. Open MySQL Workbench and choose your local connection
+
+<aside>
+💡 If you have not already, make
+
+</aside>
--- sql_in_action-starter.sql
+++ sql_in_action-starter.sql
@@ -1,27 +1,32 @@
--- EXAMPLE: SELECT ALL initial query
---    NOTE: Make sure each query ends with a semi-colon
+-- <<<<<<<<<<<<<<<<<<<<<<< EXAMPLE >>>>>>>>>>>>>>>>>>>>>>>>
+-- TODO: Remove the "--" from the below SELECT query and run the query
+--    NOTE: When writing queries, make sure each one ends with a semi-colon
 
 -- SELECT * FROM airbnb_listings_nyc;
 
--- Problem 1
+
+
+-- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 1 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find out how many rows are in the table "airbnb_listings_nyc"
 -- EXPECTED OUTPUT: 146
 
--- Problem 2
+
+-- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 2 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find out the name of the host for "host_id" 63613
 -- HINT: "Where" could it be?
 
 -- EXPECTED OUTPUT: Patricia
 
 
--- Problem 3
+-- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 3 >>>>>>>>>>>>>>>>>>>>>>>
 -- Query the data to just show the unique neighborhoods listed
 -- HINT: This is a "distinct" operation...
 
 -- EXPECTED OUTPUT: 40 neighborhoods listed
 
 
--- Problem 4
+-- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 4 >>>>>>>>>>>>>>>>>>>>>>>
+
 -- Find both the highest price listing and the lowest price listing, displaying the entire row for each
 -- HINT: This can be two different queries.
 
@@ -29,39 +34,44 @@
 
 -- EXPECTED OUTPUT: Highest = 785, Lowest = 55
 
--- Problem 5
+
+-- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 5 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find the average availability for all listings in the data set (using the availability_365 column)
 -- HINT: Aggregates are more than just big rocks...
 
 -- EXPECTED OUTPUT: 165.3904
 
--- Problem 6
+
+-- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 6 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find all listings that do NOT have a review
 -- HINT: There are a few ways to go about this. Remember that an empty cell is "no value", but not necessarily NULL
 
 -- EXPECTED OUTPUT: 6 rows
 
--- Problem 7
+
+-- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 7 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find the id of the listing with a room_type of "Private room" that has the most reviews 
 -- HINT: Sorting is your friend!
 
 -- EXPECTED OUTPUT: 58059
 
 
--- Problem 8
+-- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 8 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find the most popular neighborhood for listings 
 -- HINT: Look for which neighborhood appears most frequently in the neighbourhood column
 -- HINT: You are creating "summary rows" for each neighborhood, so you will just see one entry for each neighborhood
 
 -- EXPECTED OUTPUT: Williamsburg
 
--- Problem 9
+
+-- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 9 >>>>>>>>>>>>>>>>>>>>>>>
 -- Query the data to discover which listing is the most popular using the reviews_per_month for all listings with a minimum_nights value of less than 7
 -- HINT: Sorting is still your friend! So are constraints.
 
 -- EXPECTED OUTPUT: 58059
 
--- Problem 10
+
+-- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 10 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find out which host has the most listings. 
 -- Create a NEW column that will show a calculation for how many listings the host for each listing has in the table
 -- Display the column using aliasing.
@@ -69,10 +79,14 @@
 
 -- EXPECTED OUTPUT: The Box House Hotel with 6 listings
 
--- BONUS
+
+-- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 11 >>>>>>>>>>>>>>>>>>>>>>>
+-- <<<<<<<<<<<<<<<<<<<<<<< WRAP UP >>>>>>>>>>>>>>>>>>>>>>>>>
+-- What do you think makes a successful AirBnB rental in this market? What factors seem to be at play the most?
+-- Write a few sentances and include them with your project submission
+
+
+-- <<<<<<<<<<<<<<<<<<<<< ** BONUS ** >>>>>>>>>>>>>>>>>>>>>>>
 -- Find the the percent above or below each listing is compared to the average price for that neighborhood.
 -- HINT: No hints! It's a bonus for a reason :)
 
--- WRAP UP
--- What do you think makes a successful AirBnB rental in this market? What factors seem to be at play the most?
--- Write a few sentances and include them with your project submission
